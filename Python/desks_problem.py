42ebf97ac766ae3af86d5f176ce12c12bca15afc
Ananthakrishnan Nair RS <61831021+akrish4@users.noreply.github.com>
2020-11-10 18:09:35 +0530

    Create desks_problem.py

---
+++ Python/desks_problem.py
@@ -0,0 +1,21 @@
+# put your python code here
+g1_ = abs(int(input()))
+g2_ = abs(int(input()))
+g3_ = abs(int(input()))
+desk1 = g1_ // 2 
+desk1_ = g1_ % 2
+desk2 = g2_ // 2 
+desk2_ = g2_ % 2
+desk3 = g3_ // 2 
+desk3_ = g3_ % 2
+number_ = desk1 + desk1_ + desk2 + desk2_ + desk3 + desk3_
+print(number_)
+
+
+
+# input 
+#20
+#21
+#22
+# output 
+#32
