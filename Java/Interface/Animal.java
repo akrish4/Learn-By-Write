3783b4ff2ab8055888c1011208b3aedf81e905df
Ananthakrishnan Nair RS <61831021+akrish4@users.noreply.github.com>
2020-12-27 22:58:04 +0530

    Revert "Interface implementation"

--- Java/Interface/Animal.java
+++
@@ -1,6 +0,0 @@
-package Interface;
-// Interfaces methods cannot have bodies to insure abstraction and to provide a set of protocols throughout the code
-public interface Animal {
-    void eat();
-    void makeSound();
-}
